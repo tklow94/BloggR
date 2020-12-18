03841ebd61188f57ce9ed2ce37903ff4a2fa93fc
Trevor <tlow94@uw.edu>
2020-12-18 14:24:24 -0800

    show style

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+#disable div tag adding to input field that hits a validation error which messes up styling.
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe 
+end
+
