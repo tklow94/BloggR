c5f8b7385e4a1551a203b12a129ec6be40789e35
Trevor <tlow94@uw.edu>
2020-12-18 16:25:12 -0800

    user-article-relationship

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:notice] = "Article was created successfully" #notice and alert are the common keys used
             redirect_to @article #extracts the id from the @article object 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_234051) do
+ActiveRecord::Schema.define(version: 2020_12_19_001800) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
