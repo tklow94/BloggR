7549e3854c6d8c0fb58da0df51f19dad8822b88d
Trevor <tlow94@uw.edu>
2020-12-21 15:06:28 -0800

    add index to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
         
     end
 
+    def index
+        @users = User.all
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
