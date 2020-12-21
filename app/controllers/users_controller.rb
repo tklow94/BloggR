a6d91b1faf890895d07e27c76dbd34dff645bee5
Trevor <tlow94@uw.edu>
2020-12-21 15:31:12 -0800

    add index to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
        @user = User.find(params[:id])
        if @user.update(user_params)
             flash[:notice] = "Account Updated"
-            redirect_to articles_path
+            redirect_to @user
        else
         render 'edit'
        end
