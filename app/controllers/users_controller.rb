31c676ca65b6a48b622e95942c03d11e55e25d1c
Trevor <tlow94@uw.edu>
2020-12-22 16:45:03 -0800

    authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "#{@user.username} has signed up"
             redirect_to articles_path
         else 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
         image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
     end
 
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        # so we don't have to hit the db everytime, we check to see if we have a current user first before hitting the db.
+            
+
+    end
+
+    def logged_in?
+        !!current_user #turns current_user into a boolean. It will return true or false depending on if logged in.
+    end
+
 end
