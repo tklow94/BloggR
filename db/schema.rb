61a91d51afacae147ff7780f562d9d7eed240f75
Trevor <tlow94@uw.edu>
2020-12-29 18:00:06 -0800

    article_categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_232508) do
+ActiveRecord::Schema.define(version: 2020_12_30_015107) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
