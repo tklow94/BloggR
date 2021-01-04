b74ff9bdd6770b6c71cc47dae2f5f51a144192f6
Trevor <tlow94@uw.edu>
2021-01-04 09:57:34 -0800

    categories form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,6 +23,20 @@ class CategoriesController < ApplicationController
         end
     end
 
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice] = "Category name updated sucessfully"
+            redirect_to @category
+        else
+            render 'edit'
+        end
+    end
+
     private
     def category_params
         params.require(:category).permit(:name)
